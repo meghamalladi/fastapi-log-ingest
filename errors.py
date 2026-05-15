c72d52b8a326d7e8291b9b3ee4de640ddbbbc82f
Megha <meghanarayana22@gmail.com>
2026-05-15 16:02:09 -0400

    Changed the implementation to async and added test scripts

--- errors.py
+++ errors.py
@@ -1,17 +1,20 @@
 from fastapi.exceptions import RequestValidationError,HTTPException
 from fastapi.responses import JSONResponse
 from fastapi import Request
-from sqlalchemy.exc import ProgrammingError
+from sqlalchemy.exc import ProgrammingError,InterfaceError
 
+# If you want custom messages 
 async def log_validation_exc_handler(request:Request, exc: RequestValidationError):
-    message = "Incorrect format for one of the fields"
-    return JSONResponse(status_code=422, content=message)
+    message = "Incorrect format/fieldname for one of the fields"
+    raise HTTPException(status_code=422, detail=message)
 
 def handle_errors(e):
-    if isinstance(e, RequestValidationError):
+    if isinstance(e,ProgrammingError):
         raise
-    elif isinstance(e,ProgrammingError):
-        raise HTTPException(status_code=500, detail="Failed to add entry to the database")
+    elif isinstance(e, InterfaceError):
+        msg = f"Error Type: {type(e).__name__} - {e}. Internal interface error."
+        print(msg)
+        raise HTTPException(status_code=500, detail=msg)
     else:
         msg = f"Error Type: {type(e).__name__} - {e}"
         print(msg)
